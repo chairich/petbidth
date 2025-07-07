6cdae14fac5e1e53e1d40295514ce650653061f2
chairich <33069090+chairich@users.noreply.github.com>
2025-07-08 00:48:16 +0700

    Add MyTimer component with endTime prop

--- src/common/Timer.tsx
+++ src/common/Timer.tsx
@@ -1,49 +1,45 @@
 'use client'
-
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type TimerProps = {
-  endTime: string;
+  endTime: string | Date;
 };
 
-const MyTimer: React.FC<TimerProps> = ({ endTime }) => {
+const MyTimer = ({ endTime }: TimerProps) => {
   const calculateTimeLeft = () => {
     const difference = +new Date(endTime) - +new Date();
-    let timeLeft = {
-      days: '00',
-      hours: '00',
-      minutes: '00',
-      seconds: '00',
-    };
+    let timeLeft: any = {};
 
     if (difference > 0) {
       timeLeft = {
-        days: String(Math.floor(difference / (1000 * 60 * 60 * 24))).padStart(2, '0'),
-        hours: String(Math.floor((difference / (1000 * 60 * 60)) % 24)).padStart(2, '0'),
-        minutes: String(Math.floor((difference / 1000 / 60) % 60)).padStart(2, '0'),
-        seconds: String(Math.floor((difference / 1000) % 60)).padStart(2, '0'),
+        ชั่วโมง: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        นาที: Math.floor((difference / 1000 / 60) % 60),
+        วินาที: Math.floor((difference / 1000) % 60)
       };
+    } else {
+      timeLeft = { หมดเวลา: true };
     }
 
     return timeLeft;
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
-
     return () => clearInterval(timer);
   }, [endTime]);
 
   return (
-    <div className="bid-ends">
-      <div><span>{timeLeft.days}</span> วัน</div>
-      <div><span>{timeLeft.hours}</span> ชม.</div>
-      <div><span>{timeLeft.minutes}</span> นาที</div>
-      <div><span>{timeLeft.seconds}</span> วิ</div>
+    <div className="countdown-timer text-white small">
+      {timeLeft.หมดเวลา ? (
+        <span>หมดเวลา</span>
+      ) : (
+        <span>
+          {timeLeft.ชั่วโมง} ชม {timeLeft.นาที} นาที {timeLeft.วินาที} วิ
+        </span>
+      )}
     </div>
   );
 };
