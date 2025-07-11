cf6d8934f0d07ae91ac6565282c46fa0406a3674
chairich <33069090+chairich@users.noreply.github.com>
2025-07-11 16:32:44 +0700

    Remove broken Settings page import

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,16 +1,3 @@
-import React from "react";
-import Settings from "@/components/dashboard/settings";
-
-export const metadata = {
-	title: "Funto Settings - Next js NFT Marketplace",
-};
-
-const index = () => {
-	return (
-		<>
-			<Settings />
-		</>
-	);
-};
-
-export default index;
+export default function Page() {
+  return <div>Settings page under construction</div>;
+}
