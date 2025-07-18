ce5234228d78cb40d93345c4497ebf9dc6fe2fd3
chairich <33069090+chairich@users.noreply.github.com>
2025-07-18 20:51:45 +0700

    fix: แก้หน้า login และ authOptions

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,6 @@
-import LoginArea from '@/components/login';
+// src/app/login/page.tsx
+import React, { Suspense } from "react";
+import LoginArea from "@/components/login";
 export const metadata = {
 	title: "เข้าสู่ระบบสมาชิก - เว็บประมูล เพ็ชบิดไทย",
 };
