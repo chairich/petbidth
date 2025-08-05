4ebf3ad4dc495e26a7bc6e1999772b0cb87d6c31
chairich <33069090+chairich@users.noreply.github.com>
2025-08-05 21:51:05 +0700

    ปรับหน้าแรก page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import HomeOne from "@/components/homes/home";
 
 export const metadata = {
-	title: "แพลตฟอร์มแห่งความหวัง... - PetBidThai.com",
+  title: "ประมูลนกกรีนชีค นกสวยงาม ราคาประหยัด | PetBidThai",
+  description: "ร่วมประมูลนกกรีนชีค สายพันธุ์หายาก สีสวย เริ่มต้นเพียง 300 บาท ที่เว็บไซต์ประมูลนกอันดับ 1 ของไทย - PetBidThai.com",
 };
+
 
 const index = () => {
 	return (
