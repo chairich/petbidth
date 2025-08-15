849de84029f0ac082b751a238f0b33b5bdb0ef6a
Chairich <chairich@gmail.com>
2025-08-15 19:30:07 +0700

    add: เพิ่ม page.tsx สำหรับ เพื่อใช้ import ได้ตรง path

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import HeaderOne from '@/layouts/headers/HeaderOne';
+import FooterOne from '@/layouts/footers/FooterOne';
+import Divider from '@/components/common/Divider';
+
+export default function EditAuctionLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <HeaderOne />
+      <Divider />
+      {children}
+      <Divider />
+      <FooterOne />
+    </>
+  );
+}
